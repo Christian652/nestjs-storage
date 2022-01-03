92126aa5d6cbb70cdd9326068b6de96c3e29540e
Wesley Costa <wesleycosta.wc437@gmail.com>
2022-01-03 16:37:24 -0300

    add module settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,37 +1,36 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
-import { configService } from './config/orm'
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { ProductModule } from './product/product.module';
-import { RolesModule } from './roles/role.module';
-import { LoggerMiddleware } from './operationLogs/logger.middleware';
-import { UserController } from './user/user.controller';
-import { AuthController } from './auth/auth.controller';
-import { OperationLogsModule } from './operationLogs/operationLogs.module';
-import { ProductController } from './product/product.controller';
-import { SettingsModule } from './settings/settings.module';
-import { SettingsModule } from './settings/settings.module';
+  import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+  import { configService } from './config/orm'
+  import { TypeOrmModule } from '@nestjs/typeorm';
+  import { AppController } from './app.controller';
+  import { AppService } from './app.service';
+  import { UserModule } from './user/user.module';
+  import { AuthModule } from './auth/auth.module';
+  import { ProductModule } from './product/product.module';
+  import { RolesModule } from './roles/role.module';
+  import { LoggerMiddleware } from './operationLogs/logger.middleware';
+  import { UserController } from './user/user.controller';
+  import { AuthController } from './auth/auth.controller';
+  import { OperationLogsModule } from './operationLogs/operationLogs.module';
+  import { ProductController } from './product/product.controller';
+  import { SettingsModule } from './settings/settings.module';
 
-@Module({
-  imports: [ TypeOrmModule.forRoot(configService.getTypeOrmData()), UserModule, AuthModule, ProductModule, RolesModule, OperationLogsModule, SettingsModule],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware)
-      .exclude(
-        { path: '/users/(.*)', method: RequestMethod.GET},
-        { path: '/users', method: RequestMethod.GET }, 
-        { path: '/products/(.*)', method: RequestMethod.GET},
-        { path: '/products', method: RequestMethod.GET }, 
-      )
-      .forRoutes(UserController, AuthController, ProductController)
+  @Module({
+    imports: [ TypeOrmModule.forRoot(configService.getTypeOrmData()), UserModule, AuthModule, ProductModule, RolesModule, OperationLogsModule, SettingsModule],
+    controllers: [AppController],
+    providers: [AppService],
+  })
+  export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+      consumer
+        .apply(LoggerMiddleware)
+        .exclude(
+          { path: '/users/(.*)', method: RequestMethod.GET},
+          { path: '/users', method: RequestMethod.GET }, 
+          { path: '/products/(.*)', method: RequestMethod.GET},
+          { path: '/products', method: RequestMethod.GET }, 
+        )
+        .forRoutes(UserController, AuthController, ProductController)
+    }
   }
-}
 
 
--- src/settings/dto/settings.dto.ts
+++ src/settings/dto/settings.dto.ts
@@ -1,10 +1,16 @@
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class settingsDTO {
 
+    @IsOptional()
     id: number;
 
+    @IsString({message: 'o campo tem que ser do tipo texto'})
+    @IsNotEmpty({message: 'preencha o campo'})
     key: string;
 
+    @IsString({message: 'o campo tem que ser do tipo texto'})
+    @IsNotEmpty({message: 'preencha o campo'})
     value: string;
 
 }
